03cd71b3a4ac8b25c608406596f7b66972ffaf13
Eyas1996 <eyas.hm7@gmail.com>
2023-04-07 01:40:39 +0200

    upload exercises

--- src/09-gallery-app/App.js
+++ src/09-gallery-app/App.js
@@ -1,38 +1,13 @@
-import { useState } from "react";
-import { sculptureList } from "./Data.js";
+import React from "react";
+import Gallery from "./Gallery";
 
-export default function Gallery() {
-  let [index, setIndex] = useState(0);
-  let [details, setDetails] = useState(false);
-
-  function handleClick() {
-    setIndex(index + 1 < sculptureList.length ? index + 1 : 0);
-  }
-  function handleClickPrevious() {
-    setIndex(index <= 0 ? sculptureList.length - 1 : index - 1);
-  }
-
-  function showDetails() {
-    setDetails(!details);
-  }
-
-  let sculpture = sculptureList[index];
+const App = () => {
   return (
-    <>
-      <button onClick={handleClick}>Next</button>
-      <button onClick={handleClickPrevious}>Previous</button>
-      <h2>
-        <i>{sculpture.name} </i>
-        by {sculpture.artist}
-      </h2>
-      <h3>
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <img src={sculpture.url} alt={sculpture.alt} />
-      <button onClick={showDetails}>
-        {!details ? "Show details" : "Hidde details"}
-      </button>
-      <p>{details && sculpture.description}</p>
-    </>
+    <div className="container">
+      <Gallery />
+      <Gallery />
+    </div>
   );
-}
+};
+
+export default App;
---
+++ src/09-gallery-app/Gallery.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { sculptureList } from "./Data.js";
+
+function Gallery() {
+  let [index, setIndex] = useState(0);
+  let [details, setDetails] = useState(false);
+
+  function handleClick() {
+    setIndex(index + 1 < sculptureList.length ? index + 1 : 0);
+  }
+  function handleClickPrevious() {
+    setIndex(index <= 0 ? sculptureList.length - 1 : index - 1);
+  }
+
+  function showDetails() {
+    setDetails(!details);
+  }
+
+  let sculpture = sculptureList[index];
+  return (
+    <div className="d-flex flex-column align-items-center mx-auto w-50 pb5 pt-5">
+      <button className="mx-auto w-25 btn btn-primary mb-2" onClick={handleClick}>
+        Next
+      </button>
+      <button className="mx-auto w-25 btn btn-success mb-4" onClick={handleClickPrevious}>
+        Previous
+      </button>
+      <h2>
+        <i>{sculpture.name} </i>
+        by {sculpture.artist}
+      </h2>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <img src={sculpture.url} alt={sculpture.alt} />
+      <button className="btn btn-info mt-2" onClick={showDetails}>
+        {!details ? "Show details" : "Hidde details"}
+      </button>
+      <p>{details && sculpture.description}</p>
+      <hr />
+    </div>
+  );
+}
+export default Gallery;
